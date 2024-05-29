1ee8761fc83f0c588b15039749c5f11ae05b36bf
Oleh Duma <hakerprof@gmail.com>
2024-05-29 14:44:08 +0300

    make build

---
+++ index.d7fe9743.js
@@ -0,0 +1,2 @@
+console.log(function(s,e){for(var l=[],n="qss".split(""),t=n.length,i=s.slice(""),q=i.length,r=0;r<q;r++)i.slice(r,r+t).split("").filter(function(s){return n.includes(s)}).length===t&&l.push(r);return l}("seqsaqssesqessqeaasqseqse",0));
+//# sourceMappingURL=index.d7fe9743.js.map
